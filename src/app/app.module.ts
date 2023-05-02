bd81fba0dfd27fd21ab966bc24ece1059ce444a9
John Kennedy H. Bicbic <k.bicbic@arcanys.com>
2023-05-02 20:19:29 +0800

    added main view component and behaviour when showing sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { ViewcontainerComponent } from './components/viewcontainer/viewcontainer
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/viewcontainer/viewcontainer.component.ts
+++ src/app/components/viewcontainer/viewcontainer.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-viewcontainer',
   templateUrl: './viewcontainer.component.html',
   styleUrls: ['./viewcontainer.component.scss']
 })
+
 export class ViewcontainerComponent {
+  constructor(
+    private router: Router,
+  ) {}
+
+  isExpanded: boolean = false;
+  isTransitionShown: boolean = false;
+
+  adjustNavbar(expand: boolean): void {
+    this.isExpanded = expand;
+  }
+
+  adjustTransition(show: boolean): void {
+    this.isTransitionShown = show;
+  }
+
+  redirectToPage(link: string): void {
+    this.router.navigate([link]);
+  }
 
 }
