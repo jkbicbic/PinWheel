9599157eecf7486e917a6fa24cf5a8e7507a9896
jkbicbic <jkbicbic@gmail.com>
2025-03-16 12:30:09 +0800

    misc: refactored generate page functions

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -4,11 +4,16 @@ interface IconButtonProps extends PropsWithChildren {
   className?: string;
   icon?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-export function IconButton({ icon, children, onClick, className }: IconButtonProps) {
+export function IconButton({ icon, children, onClick, className, disabled }: IconButtonProps) {
   return (
-    <button className={`flex items-center gap-2 p-[1rem] w-fit cursor-pointer ${className}`} onClick={onClick}>
+    <button
+      className={`flex items-center gap-2 p-[1rem] w-fit cursor-pointer disabled:bg-gray-300 disabled:text-gray-500 ${className}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <img src={icon} alt="icon" />
       {children}
     </button>
--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ReactNode, useEffect, useRef, useState } from "react";
+import { ChangeEvent, ReactNode, useRef, useState } from "react";
 
 import { IconButton } from "../components/IconButton";
 import { Typography } from "../components/Typography";
@@ -17,6 +17,14 @@ export default function GeneratePage() {
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+  const setInitialState = (): void => {
+    setImage(null);
+    setPreview(null);
+    setLoading(false);
+    setIsGenerating(false);
+    setIsPalleteGenerated(false);
+  } 
+
   const onSelectImage = (): void => {
     fileInputRef.current?.click();
   };
@@ -32,66 +40,89 @@ export default function GeneratePage() {
       setTimeout(() => {
         setPreview(URL.createObjectURL(file));
         setLoading(false);
+      
       }, 500);
-    } else {
-      alert("Please select a valid image file.");
-    }
+
+      return;
+    } 
+
+    alert("Please select a valid image file.");
   };
 
   const renderImage = (): ReactNode => {
-    let imgContent = <Logo className="h-5 w-5 animate-spin" />;
-
-    if (!loading && preview) {
-      imgContent = (
-        <img className="w-full h-full object-contain rounded-lg" src={preview} alt="image to process" />
-      );
-    }
-
-    if (image) {
+    if (!isGenerating && !isPaletteGenerated && image) {
       return (
         <div className="h-[18rem] w-[18rem] bg-violet-400 rounded-2xl p-[0.625rem] flex items-center justify-center">
-          {imgContent}
+          {!loading && preview ? (
+            <img className="w-full h-full object-contain rounded-lg" src={preview} alt="image to process" />
+          ) : (
+            <Logo className="h-5 w-5 animate-spin" />
+          )}
         </div>
       );
     }
 
     return null;
   }
-  // COMMENT: wierd looking, should update this
+
   const onGeneratePallete = (): void => {
     setIsGenerating(true);
+
+    // mocked generating pallete
+    // this is where the generation
+    // code should be
+    setTimeout(() => {
+      setIsPalleteGenerated(true);
+      setImage(null);
+      setIsGenerating(false);
+    }, 4000)
   }
 
-  const renderActions = (): ReactNode => (
-    <div className="flex items-center justify-center gap-2">
-      {image && (
-        <IconButton className="rounded-full bg-purple-500 !py-[0.5rem]" icon={IconRefresh} onClick={onGeneratePallete}>
-          <Typography>Generate</Typography>
-        </IconButton> 
-      )}
+  const onSavePalette = (): void => {
+    // saving of palettes here
 
-      {isPaletteGenerated && (
-        <IconButton className="rounded-full bg-purple-500 !py-[0.5rem]" icon={IconSave} onClick={() => console.log('save palette')}>
-          <Typography>Save</Typography>
-        </IconButton> 
-      )}
-      
-      <IconButton className="rounded-full bg-gray-300 !py-[0.5rem]" icon={IconUpload} onClick={onSelectImage}>
-        <Typography className="!text-gray-500">{!image ? 'Upload' : 'Replace'}</Typography>
-      </IconButton>
-    </div>
-  )
+    setInitialState();
+  }
 
-  useEffect(() => {
-    // mocked generating palette
-    if (isGenerating) {
-      setTimeout(() => {
-        setIsPalleteGenerated(true);
-        setImage(null);
-        setIsGenerating(false);
-      }, 4000)
-    }
-  }, [isGenerating])
+  const renderActions = (): ReactNode => {
+    if (!isGenerating) {
+      return (
+        <div className="flex flex-col gap-5 justify-center">
+          {!isPaletteGenerated && !image && (
+            <Typography className="!text-gray-500 text-center">
+              Upload a photo to generate its palette
+            </Typography>
+          )}
+
+          <div className="flex items-center justify-center gap-2">
+            {image && (
+              <IconButton
+                className="rounded-full bg-purple-500 !py-[0.5rem]"
+                icon={IconRefresh}
+                onClick={onGeneratePallete}
+                disabled={loading}
+              >
+                <Typography>Generate</Typography>
+              </IconButton> 
+            )}
+
+            {isPaletteGenerated && (
+              <IconButton className="rounded-full bg-purple-500 !py-[0.5rem]" icon={IconSave} onClick={onSavePalette}>
+                <Typography>Save</Typography>
+              </IconButton> 
+            )}
+            
+            <IconButton className="rounded-full bg-purple-500 !py-[0.5rem]" icon={IconUpload} onClick={onSelectImage}>
+              <Typography>{!image ? 'Upload' : 'Replace'}</Typography>
+            </IconButton>
+          </div>
+        </div>
+      );
+    };
+
+    return null;
+  }
+
 
   return (
     <div className="w-full h-full flex flex-col gap-10 items-center justify-center">
@@ -105,8 +136,8 @@ export default function GeneratePage() {
 
       {isGenerating && <Logo className="h-10 w-10 animate-spin [&>path]:fill-purple-500" /> }
       {isPaletteGenerated && <Logo className="[&>path]:fill-purple-500 mb-10" />}
-      {!isGenerating && !isPaletteGenerated && renderImage()}
-      {!isGenerating && renderActions()}
+      {renderImage()}
+      {renderActions()}
     </div>
   )
 }
